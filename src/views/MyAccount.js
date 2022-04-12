32715c0b7a36e914fd10f314306b177df8cd410e
swagatsamal <sinu2290@gmail.com>
2022-04-12 16:59:36 -0400

    post list complete

--- src/views/MyAccount.js
+++ src/views/MyAccount.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../assets/styles/scss/style.scss";
+import defaultPic from "../assets/images/defaultCardImage.webp";
 import TopNav from "../componets/TopNav";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -9,10 +11,48 @@ import Typography from "@mui/material/Typography";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 const MyAccount = () => {
+  const navigate = useNavigate();
+
   //State variable for button text
   const [buttonText, setButtonText] = useState("Subscribe to whim");
 
-  //State variable for
+  //State variable for to store list of posts
+  const [data, setData] = useState([]);
+
+  //State variable for About author
+  const [aboutAuthor, setAboutAuthor] = useState("");
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:9032/getUserPosts", {
+        email: localStorage.getItem("email"),
+      })
+      .then((res) => {
+        if (res.data.message === "ok") {
+          setData(res.data.results);
+          setAboutAuthor(res.data.aboutAuthor);
+        } else if (res.message === "incomplete profile") {
+          navigate("/authorDetails");
+        } else {
+          alert(res.data.message);
+        }
+      });
+  }, []);
+
+  //Handle click on click of a card
+  const handleClick = (element) => {
+    localStorage.setItem("authorId", element.authorId);
+    localStorage.setItem("authorName", element.authorName);
+    localStorage.setItem("profilePic", element.authorImage);
+    localStorage.setItem("category", element.category);
+    localStorage.setItem("coverImage", element.coverImage);
+    localStorage.setItem("title", element.title);
+    localStorage.setItem("aboutCorse", element.aboutCorse);
+    localStorage.setItem("preRequisite", element.preRequisite);
+    localStorage.setItem("videoLinks", JSON.stringify(element.videoLinks));
+    navigate("/product_description");
+  };
+
   return (
     <div className="myAccountWrapper">
       <TopNav />
@@ -43,12 +83,7 @@ const MyAccount = () => {
             </div>
             <div className="card-body">
               <div className="card-content">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Officiis dicta nobis repellat! Quae eum inventore neque hic
-                  deleniti doloremque iure numquam. Incidunt dolorum sequi,
-                  placeat iste eveniet minima consequatur odio.
-                </p>
+                <p>{aboutAuthor}</p>
               </div>
             </div>
           </div>
@@ -58,28 +93,36 @@ const MyAccount = () => {
         <div className="posts">
           <div className="heading">Your posts</div>
           <div className="listView">
-            <Card sx={{ "background-color": "none" }}>
-              <CardContent>
-                <div className="postCards">
-                  <div className="cardImage">
-                    <img
-                      src={localStorage.getItem("coverImage")}
-                      alt="post image"
-                    />
-                  </div>
-                  <div className="cardTitle">
-                    <Typography variant="h6" className="heading">
-                      Post title
-                    </Typography>
-                  </div>
-                  <div className="deleteButton">
-                    <Button variant="outlined">
-                      <DeleteIcon />
-                    </Button>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
+            {data.map((element) => {
+              return (
+                <Card
+                  sx={{ backgroundColor: "none" }}
+                  key={element._id}
+                  onClick={() => handleClick(element)}
+                >
+                  <CardContent>
+                    <div className="postCards">
+                      <div className="cardImage">
+                        <img
+                          src={element.coverImage || defaultPic}
+                          alt="cover pic"
+                        />
+                      </div>
+                      <div className="cardTitle">
+                        <Typography variant="h6" className="heading">
+                          {element.title}
+                        </Typography>
+                      </div>
+                      <div className="deleteButton">
+                        <Button variant="outlined">
+                          <DeleteIcon />
+                        </Button>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              );
+            })}
           </div>
         </div>
       </div>
